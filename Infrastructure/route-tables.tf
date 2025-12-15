eb85e0012182c9909320403de499d275e2489589
im-devendhar <devendharyadav781@gmail.com>
2025-12-15 13:22:45 +0530

    Update route-tables.tf

--- Infrastructure/route-tables.tf
+++ Infrastructure/route-tables.tf
@@ -46,3 +46,13 @@ resource "aws_route_table_association" "private_rt_assoc_db_2"{
     subnet_id      = aws_subnet.private_db_2.id
     route_table_id = aws_route_table.private_rt.id
 }
+
+resource "aws_route_table_association" "private_rt_assoc_app_1"{
+    subnet_id      = aws_subnet.private_app_1.id
+    route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_rt_assoc_app_2"{
+    subnet_id      = aws_subnet.private_app_2.id
+    route_table_id = aws_route_table.private_rt.id
+}
