e2cc210f87eb048f6861ce9ee80d2bb8715eb80f
im-devendhar <devendharyadav781@gmail.com>
2025-12-15 10:41:59 +0530

    Update subnets.tf

--- Infrastructure/subnets.tf
+++ Infrastructure/subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public_1" {
     vpc_id      = aws_vpc.main.id
     cidr_block  = "10.0.1.0/24"
-    availabilty_zone = "us-east-1a"
+    availability_zone = "us-east-1a"
     map_public_ip_on_launch = true
 
     tags = {
@@ -12,7 +12,7 @@ resource "aws_subnet" "public_1" {
 resource "aws_subnet" "public_2" {
     vpc_id      = aws_vpc.main.id
     cidr_block  = "10.0.2.0/24"
-    availabilty_zone = "us-east-1b"
+    availability_zone = "us-east-1b"
     map_public_ip_on_launch = true
 
     tags = {
@@ -23,7 +23,7 @@ resource "aws_subnet" "public_2" {
 resource "aws_subnet" "private_app_1"{
     vpc_id      = aws_vpc.main.id
     cidr_block  = "10.0.3.0/24"
-    availabilty_zone = "us-east-1a"
+    availability_zone = "us-east-1a"
     
     tags = {
         name = "Private-App-Subnet-AZ1"
@@ -33,7 +33,7 @@ resource "aws_subnet" "private_app_1"{
 resource "aws_subnet" "private_app_2"{
     vpc_id      = aws_vpc.main.id
     cidr_block  = "10.0.4.0/24"
-    availabilty_zone = "us-east-1b"
+    availability_zone = "us-east-1b"
 
     tags = {
         name = "Private-App-Subnet-AZ2"
@@ -43,7 +43,7 @@ resource "aws_subnet" "private_app_2"{
 resource "aws_subnet" "private_db_1"{
     vpc_id      = aws_vpc.main.id
     cidr_block  = "10.0.5.0/24"
-    availabilty_zone = "us-east-1a"
+    availability_zone = "us-east-1a"
 
     tags = {
         name = "Private-DB-Subnet-AZ1"
@@ -53,7 +53,7 @@ resource "aws_subnet" "private_db_1"{
 resource "aws_subnet" "private_db_2"{
     vpc_id      = aws_vpc.main.id
     cidr_block  = "10.0.6.0/24"
-    availabilty_zone = "us-east-1b"
+    availability_zone = "us-east-1b"
 
     tags = {
         name = "Private-DB-Subnet-AZ2"
