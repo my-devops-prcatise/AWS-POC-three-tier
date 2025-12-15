c0f75347ca6db9ff9cc678f4c4f87c95b402720e
im-devendhar <devendharyadav781@gmail.com>
2025-12-15 13:13:13 +0530

    Update route-tables.tf

--- Infrastructure/route-tables.tf
+++ Infrastructure/route-tables.tf
@@ -7,7 +7,7 @@ resource "aws_route_table" "public_rt"{
 }
 
 resource "aws_route" "public_internet_access"{
-    route_table_id     = aws_route_table.public_rt
+    route_table_id     = aws_route_table.public_rt.id
     destination_cidr_block = "0.0.0.0/0"
     gateway_id         = aws_internet_gateway.igw.id
 
